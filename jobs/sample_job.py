5884a025eca9fbf8f396dd66d9fc2058852cb933
André Sousa <andresousa@google.com>
2022-06-20 18:29:51 +0000

    Add separated session on sample job

--- jobs/sample_job.py
+++ jobs/sample_job.py
@@ -1,6 +1,7 @@
 import logging
 
 from faker import Factory
+from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 
 import angelou.sparksession as S
@@ -20,6 +21,10 @@ def fake_int():
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
+    spark = (SparkSession.builder
+             .appName("sample-job")
+             .getOrCreate())
+
     schema = StructType([
         StructField("name", StringType(), True),
         StructField("n", IntegerType(), True),
@@ -27,7 +32,7 @@ if __name__ == "__main__":
 
     data = [(fake_name(), fake_int()) for _ in range(5)]
 
-    sample_df = S.spark.createDataFrame(
+    sample_df = spark.createDataFrame(
         data,
         schema=schema
     )
@@ -40,5 +45,3 @@ if __name__ == "__main__":
     logging.info(result_df.show(truncate=False))
 
     logging.info("jon finished!")
-
-
